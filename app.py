593925e77bb0fe9cad99c1567fb4c948814077c2
Thomas Dambrin <thomas.dambrin@gmail.com>
2024-10-04 23:23:23 -0400

    del

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+from config import OUTPUT_DIR
+
 st.title("Explore Spotify")
 
-HtmlFile = open("outputs/khruangbin_v2.html", 'r', encoding='utf-8')
+HtmlFile = open(OUTPUT_DIR / "hruangbin_v_0_1.html", 'r', encoding='utf-8')
 source_code = HtmlFile.read()
 components.html(source_code, height=1200, width=1000)
 
