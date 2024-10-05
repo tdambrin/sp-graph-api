470340678d2c2ee68fbbe43f863c4454bdd4a8c6
Thomas Dambrin <thomas.dambrin@gmail.com>
2024-10-04 23:24:35 -0400

    del

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from config import OUTPUT_DIR
 
 st.title("Explore Spotify")
 
-HtmlFile = open(OUTPUT_DIR / "hruangbin_v_0_1.html", 'r', encoding='utf-8')
+HtmlFile = open(OUTPUT_DIR / "khruangbin_v_0_2.html", 'r', encoding='utf-8')
 source_code = HtmlFile.read()
 components.html(source_code, height=1200, width=1000)
 
