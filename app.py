230d1c9ed810edfe27e93bdc416fc08acab9e702
Thomas Dambrin <thomas.dambrin@gmail.com>
2024-11-11 20:13:08 -0500

    [UPDATE] switch params to sidebar

--- app.py
+++ app.py
@@ -21,43 +21,6 @@ TYPES = {
 }
 
 
-@st.fragment
-def launch_search():
-    keywords_str = values_to_str(
-        [kw.strip() for kw in keywords.split(" ") if kw], sep="+"
-    )
-    selected_str = values_to_str(
-        [type_ for type_, is_selected in selected.items() if is_selected],
-        sep="+",
-    )
-    sp_graph_client.request(
-        method="GET", url=f"/api/search/{keywords_str}/{selected_str}"
-    )
-    search_response = sp_graph_client.getresponse()
-    body_ = json.loads(search_response.read())
-    print(f"Got body {body_}")
-    refresh_graph(nodes=body_.get("nodes"), edges=body_.get("edges"))
-
-
-@st.fragment
-def refresh_graph(
-    nodes: Optional[List[Dict[str, Any]]] = None,
-    edges: Optional[List[Dict[str, Any]]] = None,
-):
-    with placeholder.container():
-        if not keywords or nodes is None:
-            st.markdown("**Enter search keywords to compute the graph**")
-        else:
-            gv = GraphVisualizer(
-                nodes=nodes,
-                edges=edges,
-            )
-            components.html(
-                gv.html_str(),
-                height=1200,
-            )
-
-
 # --- Streamlit components ---
 
 st.set_page_config(
@@ -65,30 +28,68 @@ st.set_page_config(
     page_title="Spotify Graph",
 )
 
-# --- Title ---
-st.title("Explore through graphs")
-st.markdown("_Powered by Spotify Web API_")
-
-# --- Search params ---
-keywords = st.text_input(label="Search spotify: ")
-st.markdown(
-    """
-*Alt/Option click*: Open Spotify\\
-*Double click*: Expand graph around node
-"""
-)
-
-cols = st.columns(10)
-selected = {}
-for i, _type in enumerate(TYPES):
-    with cols[i]:
-        selected[_type] = st.checkbox(
-            _type, value=True if _type != ValidItem.ALBUM.value else False
-        )
-
 placeholder = st.empty()
 
-refresh_graph()
+with st.sidebar:
 
-if keywords:
-    launch_search()
+    @st.fragment
+    def launch_search():
+        keywords_str = values_to_str(
+            [kw.strip() for kw in keywords.split(" ") if kw], sep="+"
+        )
+        selected_str = values_to_str(
+            [type_ for type_, is_selected in selected.items() if is_selected],
+            sep="+",
+        )
+        sp_graph_client.request(
+            method="GET", url=f"/api/search/{keywords_str}/{selected_str}"
+        )
+        search_response = sp_graph_client.getresponse()
+        body_ = json.loads(search_response.read())
+        print(f"Got body {body_}")
+        refresh_graph(nodes=body_.get("nodes"), edges=body_.get("edges"))
+
+    @st.fragment
+    def refresh_graph(
+        nodes: Optional[List[Dict[str, Any]]] = None,
+        edges: Optional[List[Dict[str, Any]]] = None,
+    ):
+        with placeholder.container():
+            if not keywords or nodes is None:
+                st.markdown("**Enter search keywords to compute the graph**")
+            else:
+                gv = GraphVisualizer(
+                    nodes=nodes,
+                    edges=edges,
+                )
+                components.html(
+                    gv.html_str(),
+                    height=1200,
+                )
+
+    # --- Title ---
+    st.title("Explore through graphs")
+    st.markdown("_Powered by Spotify Web API_")
+
+    # --- Search params ---
+    keywords = st.text_input(label="Search spotify: ")
+    st.markdown(
+        """
+        *Shift clicl*: Open Spotify\\
+        *Alt/Option click*: Delete node\\
+        *Double click*: Expand graph around node
+        """
+    )
+
+    cols = st.columns(3)
+    selected = {}
+    for i, _type in enumerate(TYPES):
+        with cols[i]:
+            selected[_type] = st.checkbox(
+                _type, value=True if _type != ValidItem.ALBUM.value else False
+            )
+
+    refresh_graph()
+
+    if keywords:
+        launch_search()
